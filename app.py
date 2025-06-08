2199627260c016293fea17d8f2874c5a34bdc50f
John Wang <tmactodarack@gmail.com>
2025-06-08 01:31:38 -0500

    CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS 
 # 1. Import your Blueprint objects
 from routes.main_routes import main_bp
 from routes.blog_routes import blog_bp
@@ -7,7 +8,7 @@ from routes.macro_routes import macro_bp
 def create_app():
     """Creates and configures the Flask application."""
     app = Flask(__name__)
-
+    CORS(app) 
     # 2. Register the main blueprint
     app.register_blueprint(main_bp)
 
